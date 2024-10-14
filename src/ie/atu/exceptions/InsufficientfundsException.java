9dd8858699c27b66ad80ee4da60ff367555342f4
chauminh24 <114486211+chauminh24@users.noreply.github.com>
2024-10-14 14:42:06 +0000

    Commit 2

--- src/ie/atu/exceptions/InsufficientfundsException.java
+++ src/ie/atu/exceptions/InsufficientfundsException.java
@@ -1,5 +1,5 @@
 package ie.atu.exceptions;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class InsufficientfundsException extends Exception {
     public InsufficientfundsException(String message) {
@@ -7,25 +7,49 @@ public class InsufficientfundsException extends Exception {
     }
 }
 
-class BankAccount {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Amount want to withdraw: ");
-        double amount = scanner.nextDouble();
-        scanner.close();
-        try{
-            withdraw(amount);
-        } catch (InsufficientfundsException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+// class BankAccount {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Amount want to withdraw: ");
+//         double amount = scanner.nextDouble();
+//         scanner.close();
+//         try{
+//             withdraw(amount);
+//         } catch (InsufficientfundsException e) {
+//             System.out.println(e.getMessage());
+//         }
+//     }
     
 
-    public static void withdraw(double amount) throws InsufficientfundsException {
-        double balance = 50.99;
+//     public static void withdraw(double amount) throws InsufficientfundsException {
+//         double balance = 50.99;
+//         if (amount > balance) {
+//             throw new InsufficientfundsException("Insufficient funds for withdrawal. Available balance: " + balance);
+//         }
+//         System.out.println(("Withdraw succesfully!"));
+//     }
+// }
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(double amount) throws InsufficientfundsException {
         if (amount > balance) {
             throw new InsufficientfundsException("Insufficient funds for withdrawal. Available balance: " + balance);
         }
-        System.out.println(("Withdraw succesfully!"));
+        balance -= amount;
+
+        System.out.println("Withdrawal successfull. Remaining balance: " + balance);
     }
-}
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(100);
+
+        try {account.withdraw(150);}
+        catch (InsufficientfundsException e) {System.out.println(e.getMessage());}
+    }
+        
+    }
