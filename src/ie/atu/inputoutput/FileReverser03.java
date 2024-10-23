e3abbbb109224c30475f237d60932633719b1745
chauminh24 <114486211+chauminh24@users.noreply.github.com>
2024-10-23 12:58:18 +0000

    commit01

--- src/ie/atu/inputoutput/FileReverser03.java
+++ src/ie/atu/inputoutput/FileReverser03.java
@@ -12,7 +12,7 @@ public class FileReverser03 {
     public static void main(String[] args) {
         ArrayList<String> lines = new ArrayList<>();
         
-        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("/workspaces/week5-exceptions-lab-chauminh24/src/ie/atu/inputoutput/input.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 lines.add(line);
@@ -24,7 +24,7 @@ public class FileReverser03 {
         
         Collections.reverse(lines);
         
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("reverse.txt"))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("/workspaces/week5-exceptions-lab-chauminh24/src/ie/atu/inputoutput/reverse.txt"))) {
             for (String line : lines) {
                 writer.write(line);
                 writer.newLine();
